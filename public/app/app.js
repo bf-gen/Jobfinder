6b80035137f87dc13fce56106bc5f63bf90b356f
GenGulay <ggulay@builderfusion.com>
2015-08-25 10:01:59 +0800

    test for jobs and refactor

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,5 @@
-angular.module('app', []);
+angular.module('app', ['ngResource']);
 
-angular.module('app').controller('mainCtrl', function ($scope) {
-
-    $scope.jobs = [
-        {title: 'Sales Person', description: 'you will fight dragons'},
-        {title: 'Accountant', description: 'you will use keyboard' },
-        {title: 'Swordsman', description: 'carry sword'}
-    ];
-
+angular.module('app').controller('mainCtrl', function ($scope, $resource) {
+    $scope.jobs = $resource('/api/jobs').query();
 });
